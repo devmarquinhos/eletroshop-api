51002cd7831fb45a31d86c39ab7671c4aca5c057
devmarquinhos <marcos.emanuel.04@gmail.com>
2026-07-17 15:28:34 -0300

    :sparkles: initial commit

---
+++ src/main/java/com/devmarquinhos/eletroshop_api/EletroshopApiApplication.java
@@ -0,0 +1,13 @@
+package com.devmarquinhos.eletroshop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletroshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletroshopApiApplication.class, args);
+	}
+
+}
